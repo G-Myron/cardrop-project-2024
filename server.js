0d025ebe21dc33dfee3f2340687658b4cc70dd1c
Myron Giannakis <86615442+G-Myron@users.noreply.github.com>
2024-03-30 15:13:53 +0200

    Add host address

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/edit', (req, res) =>
 );
 
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Express-Handlebars app listening on  http://localhost:${port}/`);
 });
