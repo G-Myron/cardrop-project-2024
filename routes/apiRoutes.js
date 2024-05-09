03c01c430a9e52f7ededbc194c89ece30ecc3007
Myron Giannakis <86615442+G-Myron@users.noreply.github.com>
2024-05-09 10:44:22 +0300

    Revert "Create MongoDB project and connect it to the NodeJS project (#4)"
    This reverts commit 6fd3877b4a91e511593e657f50348182d8942f17.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,20 @@
 import express from 'express'
-import { UserController } from '../controllers/userController.js'
 
 const router = express.Router()
 
 
-router.get("/users", async (req, res) => {
-    console.log(req.params)
-    res.send(await UserController.getAllUsers())
-})
-router.get("/users/:email", async (req, res) => {
-    res.send(await UserController.getUserDetails(req.params.email))
-})
-
-router.post("/login", async (req, res) => {
-    if (await UserController.handleLogin(req.body.email, req.body.password))
-        res.redirect("/")
-    else res.redirect("/login")
-})
+router.post("/login", (req, res) => {
+    res.send(req.body)
+});
 
 router.post("/signup", (req, res) => {
     res.send(req.body)
-})
+});
 
 
 router.get("/logout", (req, res) => {
     res.send(req.body)
-})
+});
 
 router.post("/search", (req, res) => {
     console.log(req.body)
