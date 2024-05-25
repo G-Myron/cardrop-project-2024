274ffd736cbed849be47c5153dadb162517e267b
Myron Giannakis <gmyron1@gmail.com>
2024-05-26 01:09:49 +0300

    Change cars photos

--- config/initialData.js
+++ config/initialData.js
@@ -1,11 +1,11 @@
 import fs from 'fs/promises'
 
-const readFromInitFile = async (collectionName) => JSON.parse(await fs.readFile(`data/${collectionName}.json`))
+const readFromInitFile = async (collectionName) => JSON.parse(await fs.readFile(`data/init/${collectionName}.json`))
 
-const initUsers = await readFromInitFile('mockaroo/users')
-const initCategories = await readFromInitFile('init/categories')
-const initCars =  await readFromInitFile('mockaroo/cars')
-const initReservations = await readFromInitFile('mockaroo/reservations')
+const initUsers = await readFromInitFile('users')
+const initCategories = await readFromInitFile('categories')
+const initCars =  await readFromInitFile('cars')
+const initReservations = await readFromInitFile('reservations')
 
 
 export { initUsers, initCategories, initCars, initReservations }
