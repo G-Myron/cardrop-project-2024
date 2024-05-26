60bf1754e7395b53a12429b8c80a09e2cda36f7a
Myron Giannakis <gmyron1@gmail.com>
2024-05-26 20:03:10 +0300

    Fix spin loader bug

--- public/scripts/spinLoader.js
+++ public/scripts/spinLoader.js
@@ -2,17 +2,26 @@ const forms = document.querySelectorAll("form")
 const links = document.querySelectorAll("a[href]:not([target=_blank])")
 const spinLoader = document.querySelector("#spinLoader")
 
-forms.forEach( form => {
+const LOADER_DELAY = 100 //ms
+
+forms?.forEach( form => {
   form.addEventListener("submit", async (ev) => {
     if (!form.classList.contains("needs-validation"))
       showSpinner()
   })
 })
-links.forEach( link => link.addEventListener("click", showSpinner))
+links?.forEach( link => link.addEventListener("click", showSpinner))
 
 async function showSpinner() {
   setTimeout(() => {
     spinLoader.classList.remove("d-none")
     spinLoader.classList.add("d-flex")
-  }, 100)
+  }, LOADER_DELAY)
 }
+
+window.addEventListener("pageshow", ()=> {
+  setTimeout(() => {
+    spinLoader.classList.remove("d-flex")
+    spinLoader.classList.add("d-none")
+  }, 1) // or SPINLOADER_DELAY
+})
